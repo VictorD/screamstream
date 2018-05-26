58a67e299ba5f93c33212c3552fbd1222be7b6cd
Christian <mail@christiandt.com>
2018-05-26 17:11:47 +0200

    Move Mouth specifics out of main

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 from anal.analysis import Analysis
 from mouth.mouthmac import Mouth
 from chatbot.Chatbot import ChatBot
-from random import randint
 
 anal = Analysis()
 mouth = Mouth()
 bot = ChatBot()
-available_voices = ["Alex", "Fred", "Victoria", "Samantha", "Karen", "Veena", "Moira", "Fiona", "Tessa", "Daniel"]
-user_voices = {}
-
-
-def get_voice(user):
-    if user not in user_voices:
-        user_voices[user] = available_voices[randint(0, len(available_voices)-1)]
-    return user_voices[user]
 
 
 while True:
     comment, user = bot.readChat()
-    voice = get_voice(user)
+    voice = mouth.get_voice(user)
     mouth.speak(comment, voice) if anal.is_negative(comment) else None
--- mouth/mouthmac.py
+++ mouth/mouthmac.py
@@ -1,10 +1,19 @@
 from subprocess import Popen
+from random import randint
 
 
 class Mouth:
     def __init__(self):
-        pass
+        self.user_voices = {}
+        self.available_voices = ["Alex", "Fred", "Victoria", "Samantha", "Karen", "Veena", "Moira", "Fiona", "Tessa",
+                                 "Daniel"]
 
     def speak(self, sentence, voice="Alex"):
         cmd = ["say", "-v", voice, sentence]
         Popen(cmd)
+
+    def get_voice(self, user):
+        if user not in self.user_voices:
+            self.user_voices[user] = self.available_voices[randint(0, len(self.available_voices) - 1)]
+            print "user %s assigned voice %s" % (str(user), str(self.user_voices[user]))
+        return self.user_voices[user]

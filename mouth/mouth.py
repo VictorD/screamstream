1bf23e4e1cdefc3bc8c1ee80a8947b21f6094fde
Christian <mail@christiandt.com>
2018-05-26 16:02:53 +0200

    fix missing self, change variable name to match pep8

--- mouth/mouth.py
+++ mouth/mouth.py
@@ -7,15 +7,15 @@ class Mouth:
         self.engine = pyttsx.init()
         for voice in self.engine.getProperty('voices'):
             print voice
-            desiredVoice = "Cepstral Shouty"
-            if voice.name == desiredVoice:
+            desired_voice = "Cepstral Shouty"
+            if voice.name == desired_voice:
                 self.engine.setProperty('voice', voice.id)
 
     def speak(self, sentence):
         self.engine.say(sentence)
         self.engine.runAndWait()
     
-    def close():
+    def close(self):
         self.engine.stop()
 
 

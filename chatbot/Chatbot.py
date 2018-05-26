b01e3e0c7f3bad1317e67cbebc7806d12b1035ec
victord <victor.danell@gmail.com>
2018-05-26 13:48:03 +0200

    Fix self

--- chatbot/Chatbot.py
+++ chatbot/Chatbot.py
@@ -3,30 +3,23 @@ from Read import getUser, getMessage
 from Socket import openSocket, sendMessage
 from Initialize import joinRoom
 
-from threading import Event
-
-exit = Event()
-
-
 class ChatBot:
 
-    def __init__():
-        s = openSocket()
-        joinRoom(s)
+    def __init__(self):
+        self.s = openSocket()
+        joinRoom(self.s)
 
-    def readChat():
-        readbuffer = readbuffer + s.recv(1024)
+    def readChat(self):
+        readbuffer =  self.s.recv(1024)
         temp = string.split(readbuffer, "\n")
         readbuffer = temp.pop()
         
         for line in temp:
             print(line)
             if "PING" in line:
-                s.send(line.replace("PING", "PONG"))
+                self.s.send(line.replace("PING", "PONG"))
                 break
             user = getUser(line)
             message = getMessage(line)
             print user + " typed :" + message
-            if "You Suck" in message:
-                sendMessage(s, "No, you suck!")
-                break
+            return message
